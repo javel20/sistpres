f9045c573ffddce9ac9ce06d6c87b3958d1fd164
javier elias <javier_el_balla@hotmail.com>
2023-11-01 09:50:15 -0500

    modelo usuario

--- modelos/usuarioModelo.php
+++ modelos/usuarioModelo.php
@@ -4,6 +4,22 @@
 
     class usuarioModelo extends MainModel{
         
+        protected static function agregar_usuario_modelo($datos){
+            $sql=mainModel::conectar()->prepare("INSERT INTO usuario(usuario_dni,usuario_nombre,usuario_apellido,usuario_telefono,
+            usuario_direccion,usuario_email,usuario_usuario,usuario_clave) VALUES (:DNI,:Nombre,:Apellido,:Telefono,:Direccion,:Email,:Usuario,:Clave)");
+   
+            $sql->bindParam(":DNI",$datos['DNI']);
+            $sql->bindParam(":Nombre",$datos['Nombre']);
+            $sql->bindParam(":Apellido",$datos['Apellido']);
+            $sql->bindParam(":Telefono",$datos['Telefono']);
+            $sql->bindParam(":Direccion",$datos['Direccion']);
+            $sql->bindParam(":Email",$datos['Email']);
+            $sql->bindParam(":Usuario",$datos['Usuario']);
+            $sql->bindParam(":Clave",$datos['Clave']);
+            $sql->bindParam(":Estado",$datos['Estado']);
+            $sql->bindParam(":Privilegio",$datos['Privilegio']);
+            $sql->execute();
+        }
     }
 
 
